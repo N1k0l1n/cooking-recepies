3673667137feb76d39da322e8e2c3cef0990ad7b
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-10-16 16:11:10 +0200

    tested search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
 
   return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
       <div>
         <FaSearch></FaSearch>
         <input
