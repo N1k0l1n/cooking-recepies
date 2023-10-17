dea3d260ac84587c4dbddf6840ed80f7f540aafe
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-10-17 10:44:14 +0200

    final touches (add motion)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Vegie from "../components/Vegie";
 import Popular from "../components/Popular";
+import {motion} from 'framer-motion';
 
 function Home() {
   return (
-    <div>
+    <motion.div
+    animate={{ opacity: 1 }}
+    initial={{ opacity: 0 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+    >
       <Vegie />
       <Popular />
-    </div>
+    </motion.div>
   );
 }
 
